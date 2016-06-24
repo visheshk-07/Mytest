914e6320954e1e65844505fcd6d020bb27488829
visheshk-07 <visheshkumar412@gmail.com>
2016-06-24 15:50:56 +0530

    SocialLoginSample

---
+++ SocialLoginSample/app/src/main/java/auth0/com/socialloginsample/LoginActivity.java
@@ -0,0 +1,329 @@
+package auth0.com.socialloginsample;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import io.fabric.sdk.android.Fabric;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via multiple providers.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 9001;
+
+    private static EditText username;
+    private static EditText password;
+
+    private static ImageButton login_btn;
+
+    // UI references.
+    private SignInButton mGoogleSignInButton;
+    private LoginButton mFacebookSignInButton;
+    private TwitterLoginButton mTwitterSignInButton;
+    private Button mInstagramSignInButton;
+
+    // Vars
+    private GoogleApiClient mGoogleApiClient;
+    private CallbackManager mFacebookCallbackManager;
+
+    
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        username =(EditText)findViewById(R.id.etUsername);
+        password =(EditText)findViewById(R.id.etPassword);
+
+        login_btn = (ImageButton)findViewById(R.id.ibLogin);
+
+
+        Loginbutton();
+
+
+
+
+
+
+
+
+
+
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        mFacebookCallbackManager = CallbackManager.Factory.create();
+
+        checkForInstagramData();
+
+        // The private key that follows should never be public
+        // (consider this when deploying the application)
+        TwitterAuthConfig authConfig = new TwitterAuthConfig("lPcEPVTOHSdQgfy22rxYlvz04",
+                "Rd9yQ4B8dSffj025UJP8y3QQIbJvRO6eUv68jmgIhe1dUSdjNq");
+        Fabric.with(this, new TwitterCore(authConfig));
+
+        setContentView(R.layout.activity_login);
+
+        mGoogleSignInButton = (SignInButton)findViewById(R.id.google_sign_in_button);
+        mGoogleSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInWithGoogle();
+            }
+        });
+
+        mFacebookSignInButton = (LoginButton)findViewById(R.id.facebook_sign_in_button);
+        mFacebookSignInButton.registerCallback(mFacebookCallbackManager,
+            new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(final LoginResult loginResult) {
+                    handleSignInResult(new Callable<Void>() {
+                        @Override
+                        public Void call() throws Exception {
+                            LoginManager.getInstance().logOut();
+                            return null;
+                        }
+                    });
+                }
+
+                @Override
+                public void onCancel() {
+                    handleSignInResult(null);
+                }
+
+                @Override
+                public void onError(FacebookException error) {
+                    Log.d(LoginActivity.class.getCanonicalName(), error.getMessage());
+                    handleSignInResult(null);
+                }
+            }
+        );
+
+        mTwitterSignInButton = (TwitterLoginButton)findViewById(R.id.twitter_sign_in_button);
+        mTwitterSignInButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(final Result<TwitterSession> result) {
+                handleSignInResult(new Callable<Void>() {
+                    @Override
+                    public Void call() throws Exception {
+                        TwitterCore.getInstance().logOut();
+                        return null;
+                    }
+                });
+            }
+
+            @Override
+            public void failure(TwitterException e) {
+                Log.d(LoginActivity.class.getCanonicalName(), e.getMessage());
+                handleSignInResult(null);
+            }
+        });
+
+        mInstagramSignInButton = (Button)findViewById(R.id.instagram_sign_in_button);
+        mInstagramSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInWithInstagram();
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if(result.isSuccess()) {
+                final GoogleApiClient client = mGoogleApiClient;
+
+                handleSignInResult(new Callable<Void>() {
+                    @Override
+                    public Void call() throws Exception {
+                        if (client != null) {
+
+                            Auth.GoogleSignInApi.signOut(client).setResultCallback(
+                                new ResultCallback<Status>() {
+                                    @Override
+                                    public void onResult(Status status) {
+                                        Log.d(LoginActivity.class.getCanonicalName(),
+                                                status.getStatusMessage());
+
+                                        /* TODO: handle logout failures */
+                                    }
+                                }
+                            );
+
+                        }
+
+                        return null;
+                    }
+                });
+
+            } else {
+                handleSignInResult(null);
+            }
+        } else if(TwitterAuthConfig.DEFAULT_AUTH_REQUEST_CODE == requestCode) {
+            mTwitterSignInButton.onActivityResult(requestCode, resultCode, data);
+        } else {
+            mFacebookCallbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void signInWithGoogle() {
+        if(mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        final Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signInWithInstagram() {
+        final Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder.scheme("https")
+                  .authority("api.instagram.com")
+                  .appendPath("oauth")
+                  .appendPath("authorize")
+                  .appendQueryParameter("client_id", "18a8b74da9644bd7a9294caef1c5e76c")
+                  .appendQueryParameter("redirect_uri", "sociallogin://redirect")
+                  .appendQueryParameter("response_type", "token");
+        final Intent browser = new Intent(Intent.ACTION_VIEW, uriBuilder.build());
+        startActivity(browser);
+    }
+
+    private void checkForInstagramData() {
+        final Uri data = this.getIntent().getData();
+        if(data != null && data.getScheme().equals("sociallogin") && data.getFragment() != null) {
+            final String accessToken = data.getFragment().replaceFirst("access_token=", "");
+            if (accessToken != null) {
+                handleSignInResult(new Callable<Void>() {
+                    @Override
+                    public Void call() throws Exception {
+                        // Do nothing, just throw the access token away.
+                        return null;
+                    }
+                });
+            } else {
+                handleSignInResult(null);
+            }
+        }
+    }
+
+    private void handleSignInResult(Callable<Void> logout) {
+        if(logout == null) {
+            /* Login error */
+            Toast.makeText(getApplicationContext(), R.string.login_error, Toast.LENGTH_SHORT).show();
+        } else {
+            /* Login success */
+            Application.getInstance().setLogoutCallable(logout);
+            startActivity(new Intent(this, LoggedInActivity.class));
+        }
+    }
+
+    public void clickHandler(View view) {
+    }
+
+    public void Loginbutton() {
+
+        login_btn.setOnClickListener(
+
+                new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                        if(username.getText().toString().equals("vishesh")
+                                && password.getText().toString().equals("password"))
+                        {
+
+                            Toast.makeText(LoginActivity.this,"User and password is correct",
+                                    Toast.LENGTH_SHORT).show();
+                                    Intent i = new Intent("auth0.com.socialloginsample.NavigationDrawer");
+                                    startActivity(i);
+                        }
+
+                        else{
+
+
+                            Toast.makeText(LoginActivity.this,"User and password is not correct",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+
+
+    }
+}
+
---
+++ SocialLoginSample/app/src/main/java/auth0/com/socialloginsample/NavigationDrawer.java
@@ -0,0 +1,19 @@
+package auth0.com.socialloginsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class NavigationDrawer extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
